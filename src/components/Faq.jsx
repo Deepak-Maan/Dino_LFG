f0bee31284d556b138ab5c90e158b7a0be183804
Deepak-Maan <jattdevta1008@gmail.com>
2024-02-12 10:52:24 +0530

    faq section modified

--- src/components/Faq.jsx
+++ src/components/Faq.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Accordion, AccordionHeader, AccordionBody, } from "@material-tailwind/react";
 import Faqlayer1 from '../assets/images/faqlayer1.webp';
-function Icon({ id, open }) {
+function Arrow({ id, open }) {
     return (
         <div className="relative ">
             <svg className={`${id === open ? "rotate-180" : "rotate-0"}  sm:mr-12   transition-transform`} width="36" height="36" viewBox="0 0 36 36" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -16,31 +16,31 @@ function Icon({ id, open }) {
 function Faq() {
     const [open, setOpen] = React.useState(1);
 
-    const handleOpen = (value) => setOpen(open === value ? 0 : value);
+    const AccordionOpen = (value) => setOpen(open === value ? 0 : value);
 
     return (
         <div id="faq" className="relative">
             <div className="max-w-[1140px] mx-auto px-3  lg:pt-[310px] md:pt-[200px] sm:pt-[150px] pt-[100px]">
                 <h2 className='sm:text-[64px] text-[40px] leading-[132%] text-center font-normal font-chewy text-white lg:mb-[61px] mb-[35px]'>FAQs</h2>
                 <div data-aos-delay="400" data-aos="zoom-in" className=" mx-auto relative max-w-[800px] px-2 ">
-                    <Accordion className={`border-white border bg-[#09655A] mb-6 rounded-full p-3 sm:pt-[30px] sm:pb-[27px] sm:ps-[26px] ${open === 1 ? 'rounded-[26px] sm:rounded-[45px] ' : 'rounded-full'}`} open={open === 1} icon={<Icon id={1} open={open} />}                >
-                        <AccordionHeader onClick={() => handleOpen(1)} className="font-balsamiq text-[11px] sm:text-[16px] md:text-[20px] text-start font-bold text-white w-full justify-between" >Maecenas laoreet, sapien vel cursus ultricies?</AccordionHeader>
+                    <Accordion onClick={() => AccordionOpen(1)} className={`border-white cursor-pointer border bg-[#09655A] mb-6 rounded-full p-3 sm:pt-[30px] sm:pb-[27px] sm:ps-[26px] ${open === 1 ? 'rounded-[26px] sm:rounded-[45px] ' : 'rounded-full'}`} open={open === 1} icon={<Arrow id={1} open={open} />}                >
+                        <AccordionHeader className="font-balsamiq text-[11px] sm:text-[16px] md:text-[20px] text-start font-bold text-white w-full justify-between" >Maecenas laoreet, sapien vel cursus ultricies?</AccordionHeader>
                         <AccordionBody className="font-balsamiq text-[14px] md:text-[16px] font-medium text-white max-w-[600px] pt-3 ">Maecenas laoreet, sapien vel cursus ultricies? Maecenas laoreet, sapien vel cursus ultricies? Maecenas laoreet, sapien vel cursus ultricies? Maecenas laoreet, sapien vel cursus ultricies? </AccordionBody>
                     </Accordion>
-                    <Accordion className={`border-white border bg-[#09655A] mb-6 overflow-hidden rounded-full p-3 sm:pt-[30px] sm:pb-[27px] sm:ps-[26px] ${open === 2 ? 'rounded-[26px] sm:rounded-[45px]' : 'rounded-full'}`} open={open === 2} icon={<Icon id={2} open={open} />} >
-                        <AccordionHeader onClick={() => handleOpen(2)} className="font-balsamiq text-[11px] sm:text-[16px] md:text-[20px] text-start font-bold text-white"> Maecenas laoreet, sapien vel cursus ultricies?</AccordionHeader>
+                    <Accordion onClick={() => AccordionOpen(2)} className={`border-white cursor-pointer border bg-[#09655A] mb-6 overflow-hidden rounded-full p-3 sm:pt-[30px] sm:pb-[27px] sm:ps-[26px] ${open === 2 ? 'rounded-[26px] sm:rounded-[45px]' : 'rounded-full'}`} open={open === 2} icon={<Arrow id={2} open={open} />} >
+                        <AccordionHeader className="font-balsamiq text-[11px] sm:text-[16px] md:text-[20px] text-start font-bold text-white"> Maecenas laoreet, sapien vel cursus ultricies?</AccordionHeader>
                         <AccordionBody className="font-balsamiq text-[14px] md:text-[16px] font-medium text-white max-w-[600px] pt-3 ">Maecenas laoreet, sapien vel cursus ultricies? Maecenas laoreet, sapien vel cursus ultricies? Maecenas laoreet, sapien vel cursus ultricies? Maecenas laoreet, sapien vel cursus ultricies? </AccordionBody>
                     </Accordion>
-                    <Accordion className={`border-white border bg-[#09655A] mb-6 rounded-full p-3 sm:pt-[30px] sm:pb-[27px] sm:ps-[26px] ${open === 3 ? 'rounded-[26px] sm:rounded-[45px]' : 'rounded-full'}`} open={open === 3} icon={<Icon id={3} open={open} />}>
-                        <AccordionHeader onClick={() => handleOpen(3)} className="font-balsamiq text-[11px] sm:text-[16px] md:text-[20px] text-start font-bold text-white" >Maecenas laoreet, sapien vel cursus ultricies?</AccordionHeader>
+                    <Accordion onClick={() => AccordionOpen(3)} className={`border-white cursor-pointer border bg-[#09655A] mb-6 rounded-full p-3 sm:pt-[30px] sm:pb-[27px] sm:ps-[26px] ${open === 3 ? 'rounded-[26px] sm:rounded-[45px]' : 'rounded-full'}`} open={open === 3} icon={<Arrow id={3} open={open} />}>
+                        <AccordionHeader className="font-balsamiq text-[11px] sm:text-[16px] md:text-[20px] text-start font-bold text-white" >Maecenas laoreet, sapien vel cursus ultricies?</AccordionHeader>
                         <AccordionBody className="font-balsamiq text-[14px] md:text-[16px] font-medium text-white max-w-[600px] pt-3 ">Maecenas laoreet, sapien vel cursus ultricies? Maecenas laoreet, sapien vel cursus ultricies? Maecenas laoreet, sapien vel cursus ultricies? Maecenas laoreet, sapien vel cursus ultricies? </AccordionBody>
                     </Accordion>
-                    <Accordion className={`border-white border bg-[#09655A] mb-6 rounded-full p-3 sm:pt-[30px] sm:pb-[27px] sm:ps-[26px] ${open === 4 ? 'rounded-[26px] sm:rounded-[45px]' : 'rounded-full'}`} open={open === 4} icon={<Icon id={4} open={open} />}>
-                        <AccordionHeader onClick={() => handleOpen(4)} className="font-balsamiq text-[11px] sm:text-[16px] md:text-[20px] text-start font-bold text-white">Maecenas laoreet, sapien vel cursus ultricies?</AccordionHeader>
+                    <Accordion onClick={() => AccordionOpen(4)} className={`border-white cursor-pointer border bg-[#09655A] mb-6 rounded-full p-3 sm:pt-[30px] sm:pb-[27px] sm:ps-[26px] ${open === 4 ? 'rounded-[26px] sm:rounded-[45px]' : 'rounded-full'}`} open={open === 4} icon={<Arrow id={4} open={open} />}>
+                        <AccordionHeader className="font-balsamiq text-[11px] sm:text-[16px] md:text-[20px] text-start font-bold text-white">Maecenas laoreet, sapien vel cursus ultricies?</AccordionHeader>
                         <AccordionBody className="font-balsamiq text-[14px] md:text-[16px] font-medium text-white max-w-[600px] pt-3 ">Maecenas laoreet, sapien vel cursus ultricies? Maecenas laoreet, sapien vel cursus ultricies? Maecenas laoreet, sapien vel cursus ultricies? Maecenas laoreet, sapien vel cursus ultricies? </AccordionBody>
                     </Accordion>
-                    <Accordion className={`border-white border bg-[#09655A] mb-6 rounded-full p-3 sm:pt-[30px] sm:pb-[27px] sm:ps-[26px] ${open === 5 ? 'rounded-[26px] sm:rounded-[45px]' : 'rounded-full'}`} open={open === 5} icon={<Icon id={5} open={open} />}>
-                        <AccordionHeader onClick={() => handleOpen(5)} className="font-balsamiq text-[11px] sm:text-[16px] md:text-[20px] text-start font-bold text-white">Maecenas laoreet, sapien vel cursus ultricies?</AccordionHeader>
+                    <Accordion onClick={() => AccordionOpen(5)} className={`border-white cursor-pointer border bg-[#09655A] mb-6 rounded-full p-3 sm:pt-[30px] sm:pb-[27px] sm:ps-[26px] ${open === 5 ? 'rounded-[26px] sm:rounded-[45px]' : 'rounded-full'}`} open={open === 5} icon={<Arrow id={5} open={open} />}>
+                        <AccordionHeader className="font-balsamiq text-[11px] sm:text-[16px] md:text-[20px] text-start font-bold text-white">Maecenas laoreet, sapien vel cursus ultricies?</AccordionHeader>
                         <AccordionBody className="font-balsamiq text-[14px] md:text-[16px] font-medium text-white max-w-[600px] pt-3 ">Maecenas laoreet, sapien vel cursus ultricies? Maecenas laoreet, sapien vel cursus ultricies? Maecenas laoreet, sapien vel cursus ultricies? Maecenas laoreet, sapien vel cursus ultricies? </AccordionBody>
                     </Accordion>
                 </div>
